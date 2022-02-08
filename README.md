5ca81ade8fe77df48e7093e91533fc785d07b108
Joey <786678257@qq.com>
2022-02-08 17:27:49 +0800

    init🎉:  init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # blog
-blog
+
+- <https://mdxjs.com/>
+
+- <https://github.com/gitalk/gitalk#install>
+
+- https://github.com/parksben/markdown-navbar
+
+- https://github.com/remarkjs/react-markdown
+
+- https://github.com/jonschlinkert/gray-matter
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Button } from 'antd'
+
+const Home: NextPage = () => {
+  return (
+    <div className='text-center'>
+      <Button type="primary">Button</Button>
+    </div>
+  )
+}
+
+export default Home
